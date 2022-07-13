c0ac1410e58b10f0cafac5469899ad63c864af07
Rumpels95 <joan.quispec@gmail.com>
2022-07-13 15:34:15 -0500

    add navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import LandingPage from "./pages/LandingPage/LandingPage.jsx";
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Router>
+        <Navbar />
+        <div className="app__container">
+          <Routes>
+            <Route exact path="/" component={LandingPage} />
+            {/* <Route exact path="/home" component={Home} />
+            <Route path="/recipe" component={CreateFood} />
+            <Route path="/recipes/:id" component={Detail} /> */}
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import ReorderIcon from "@material-ui/icons/Reorder";
+import { Link } from 'react-scroll'
+import './Navbar.css'
+
+function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation();
+  const myRef = []
+
+  useEffect(() => {
+    setExpandNavbar(false);
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return (
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <div>
+          <img/>
+        </div>
+        <ul className='navbar-nav'>
+          <li className="nav-item active">
+            <Link activeClass="active" smooth spy to="about">
+                HOME
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link activeClass="active" smooth spy to="skills">
+                SKILLS
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link activeClass="active" smooth spy to="projects">
+                PROJECTS
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link activeClass="active" smooth spy to="experience">
+                EXPERIENCE
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
